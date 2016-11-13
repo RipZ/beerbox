bb89a0800c77d7ec614fe8955f8dd7573b34c485
MegaRipZ <v.sadilov@gmail.com>
2016-11-13 13:21:24 +0300

    Add fonts and rewrite path's

--- scripts/digitemp.sh
+++ scripts/digitemp.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-# detect devices on 1w-bus and write config
-digitemp_DS9097U -i -s /dev/ttyS1 -c /root/digitemp.conf
-
-# read device's data on 1w-bus
-#digitemp_DS9097U -a -s /dev/ttyS1 -c /root/digitemp.conf
-
-# read one device -t 0
-#digitemp_DS9097U -t 0 -q -c /root/digitemp.conf
+/opt/beerbox/bin/digitemp.sh
